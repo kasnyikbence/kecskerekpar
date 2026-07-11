1045c3e961f6398d457b274b7525db22c3d14acf
Kasnyik Bence <h263908@stud.u-szeged.hu>
2026-07-11 13:27:00 +0200

    animációk létrehozása és stílus egységesítése

--- src/app/components/main-component/main-component.ts
+++ src/app/components/main-component/main-component.ts
@@ -1,18 +1,36 @@
 import { Component, inject } from '@angular/core';
-import { NavComponent } from "../nav-component/nav-component";
-import { MatIconModule } from "@angular/material/icon";
+import { NavComponent } from '../nav-component/nav-component';
+import { MatIconModule } from '@angular/material/icon';
 import { Router } from '@angular/router';
 
 @Component({
-  selector: 'app-main-component',
-  imports: [MatIconModule],
-  templateUrl: './main-component.html',
-  styleUrl: './main-component.scss',
+    selector: 'app-main-component',
+    imports: [MatIconModule],
+    templateUrl: './main-component.html',
+    styleUrl: './main-component.scss',
 })
 export class MainComponent {
-  private router = inject(Router);
+    private router = inject(Router);
 
-      navigateTo(route: string): void {
+    ngAfterViewInit(): void {
+        const observer = new IntersectionObserver(
+            entries => {
+                entries.forEach(entry => {
+                    if (entry.isIntersecting) {
+                        entry.target.classList.add('show');
+                    } else {
+                        entry.target.classList.remove('show');
+                    }
+                });
+            },
+            { threshold: 0.1 },
+        );
+
+        const sections = document.querySelectorAll('section');
+        sections.forEach(element => observer.observe(element));
+    }
+
+    navigateTo(route: string): void {
         this.router.navigateByUrl(route);
     }
 }
